87f4713d3d7b4e4d67a3c7352d1b26ff29a1b171
k1shor <k1shor@hotmail.com>
2024-05-21 13:24:53 +0545

    21st may

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel')
 const Token = require('../models/tokenModel')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
+const sendEmail = require('../utils/emailSender')
 const saltRounds = 10
 
 
@@ -23,25 +24,56 @@ exports.userRegister = async (req, res) => {
     let salt = await bcrypt.genSalt(saltRounds)
     let hashed_password = await bcrypt.hash(password, salt)
 
-    let user = await User.create({ username, email, password:hashed_password })
-    if(!user){
-        return res.status(400).json({error:"Something went wrong"})
+    let user = await User.create({ username, email, password: hashed_password })
+    if (!user) {
+        return res.status(400).json({ error: "Something went wrong" })
     }
-// generate token
+    // generate token
     let token = await Token.create({
         token: crypto.randomBytes(16).toString('hex'),
         user: user._id
     })
-    if(!token){
-        return res.status(400).json({error:"Something went wrong"})
+    if (!token) {
+        return res.status(400).json({ error: "Something went wrong" })
     }
-    
 
+    let URL = `http://localhost:5000/verifyuser/${token.token}`
 
-// send token in email
-
-
-
+    // send token in email
+    sendEmail({
+        from: 'noreply@something.com',
+        to: email,
+        subject: "Verification Email",
+        text: `Click on the following link, or copy paste it in browser to verify your account ${URL}`,
+        html: `<a href='${URL}'><button>Verify Email</button></a>`
+    })
 
     res.send(user)
+}
+
+// verify email 
+exports.verifyEmail = async (req, res) => {
+    // check token if valid or not
+    let token = await Token.findOne({token: req.params.token})
+    if(!token){
+        return res.status(400).json({error:"Invalid or token may have expired"})
+    }
+    // find user
+    let user = await User.findById(token.user)
+    if(!user){
+        return res.status(400).json({error:"User not found"})
+    }
+    // check if already verified
+    if(user.isVerified){
+        return res.status(400).json({error:"User already verified, login to continue"})
+    }
+
+    // verify user
+    user.isVerified = true
+    user = await user.save()
+    if(!user){
+        return res.status(400).json({error:"Failed to verify. Try again later"})
+    }
+
+    res.send({message: "User verified successfully"})
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const { userRegister } = require('../controller/userController')
+const { userRegister, verifyEmail } = require('../controller/userController')
 
 
 router.post('/register', userRegister)
+router.get('/verifyuser/:token', verifyEmail)
 
 
 module.exports = router
